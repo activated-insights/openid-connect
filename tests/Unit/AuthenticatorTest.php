c1284adaa2ad308340e65c5f99f877d20d0737c5
zackrowe <4922356+zackrowe@users.noreply.github.com>
2022-02-01 10:19:07 -0700

    Add additional test to AuthenticatorTest

--- tests/Unit/AuthenticatorTest.php
+++ tests/Unit/AuthenticatorTest.php
@@ -2,7 +2,9 @@
 
 namespace Unit;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use Pinnacle\OpenIdConnect\Authentication\AuthenticationUriBuilder;
 use Pinnacle\OpenIdConnect\Authentication\Constants\StateKey;
@@ -12,14 +14,20 @@ use Pinnacle\OpenIdConnect\Authentication\StatePersister\Contracts\StatePersiste
 use Pinnacle\OpenIdConnect\Authenticator;
 use Pinnacle\OpenIdConnect\Authorization\AuthorizationCodeResponse;
 use Pinnacle\OpenIdConnect\Authorization\Models\AuthorizationCode;
+use Pinnacle\OpenIdConnect\Authorization\TokensResponse;
 use Pinnacle\OpenIdConnect\Provider\Models\ClientId;
 use Pinnacle\OpenIdConnect\Provider\Models\ClientSecret;
 use Pinnacle\OpenIdConnect\Provider\Models\Identifier;
 use Pinnacle\OpenIdConnect\Provider\Models\ProviderConfiguration;
 use Pinnacle\OpenIdConnect\Support\Exceptions\InsecureUriException;
+use Pinnacle\OpenIdConnect\Support\Traits\GenerateUserIdJwt;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
 
 class AuthenticatorTest extends TestCase
 {
+    use GenerateUserIdJwt;
+
     /**
      * @test
      */
@@ -113,25 +121,25 @@ class AuthenticatorTest extends TestCase
         $statePersister = $this->getMockBuilder(StatePersisterInterface::class)->getMock();
 
         $statePersister->expects($this->exactly(7))
-                       ->method('getValue')
-                       ->withConsecutive(
-                           [StateKey::CHALLENGE()->withPrefix($state->getValue())],
-                           [StateKey::PROVIDER_IDENTIFIER()->withPrefix($state->getValue())],
-                           [StateKey::PROVIDER_CLIENT_ID()->withPrefix($state->getValue())],
-                           [StateKey::PROVIDER_CLIENT_SECRET()->withPrefix($state->getValue())],
-                           [StateKey::PROVIDER_AUTHORIZATION_ENDPOINT()->withPrefix($state->getValue())],
-                           [StateKey::PROVIDER_TOKEN_ENDPOINT()->withPrefix($state->getValue())],
-                           [StateKey::REDIRECT_URI()->withPrefix($state->getValue())]
-                       )
-                       ->willReturnOnConsecutiveCalls(
-                           $challenge->getValue(),
-                           $identifier->getValue(),
-                           $clientId->getValue(),
-                           $clientSecret->getValue(),
-                           (string)$authorizationEndpoint,
-                           (string)$tokenEndpoint,
-                           (string)$secureRedirectUri,
-                       );
+            ->method('getValue')
+            ->withConsecutive(
+                [StateKey::CHALLENGE()->withPrefix($state->getValue())],
+                [StateKey::PROVIDER_IDENTIFIER()->withPrefix($state->getValue())],
+                [StateKey::PROVIDER_CLIENT_ID()->withPrefix($state->getValue())],
+                [StateKey::PROVIDER_CLIENT_SECRET()->withPrefix($state->getValue())],
+                [StateKey::PROVIDER_AUTHORIZATION_ENDPOINT()->withPrefix($state->getValue())],
+                [StateKey::PROVIDER_TOKEN_ENDPOINT()->withPrefix($state->getValue())],
+                [StateKey::REDIRECT_URI()->withPrefix($state->getValue())]
+            )
+            ->willReturnOnConsecutiveCalls(
+                $challenge->getValue(),
+                $identifier->getValue(),
+                $clientId->getValue(),
+                $clientSecret->getValue(),
+                (string)$authorizationEndpoint,
+                (string)$tokenEndpoint,
+                (string)$secureRedirectUri,
+            );
 
         $authorizationCode = new AuthorizationCode('authorization-code');
 
@@ -165,6 +173,60 @@ class AuthenticatorTest extends TestCase
      */
     public function fetchTokensWithAuthorizationCode_WithValidValues_ReturnsTokensResponseObject(): void
     {
-        $this->markTestIncomplete();
+        // Assemble
+        $authorizationCode     = new AuthorizationCode('fake-authorization-code');
+        $identifier            = new Identifier('identifier');
+        $clientId              = new ClientId('client-id');
+        $clientSecret          = new ClientSecret('client-secret');
+        $authorizationEndpoint = new Uri('https://endpoint.test/authorization');
+        $tokenEndpoint         = new Uri('https://endpoint.test/token');
+
+        $provider = new ProviderConfiguration(
+            $identifier,
+            $clientId,
+            $clientSecret,
+            $authorizationEndpoint,
+            $tokenEndpoint
+        );
+
+        $redirectUri = new Uri('https//endpoint.test/redirect');
+
+        $challenge = Challenge::createWithRandomString();
+
+        $authorizationCodeResponse = new AuthorizationCodeResponse(
+            $authorizationCode,
+            $provider,
+            $redirectUri,
+            $challenge
+        );
+
+        $streamInterface = Mockery::spy(StreamInterface::class);
+        $streamInterface->shouldReceive('getContents')
+                        ->andReturn(
+                            json_encode(
+                                [
+                                    'id_token'     => $this->generateRandomJwt(),
+                                    'access_token' => 'fake-access-token',
+                                ]
+                            )
+                        );
+
+        $requestMock = Mockery::spy(RequestInterface::class);
+        $requestMock->shouldReceive('getBody')
+                    ->andReturn($streamInterface);
+
+        $clientMock = Mockery::spy('overload:' . Client::class);
+        $clientMock->shouldReceive('request')
+                   ->andReturn($requestMock);
+
+        $statePersister = $this->getMockBuilder(StatePersisterInterface::class)->getMock();
+
+        $authenticator = new Authenticator($statePersister);
+
+        // Act
+        $tokensResponse = $authenticator->fetchTokensWithAuthorizationCode($authorizationCodeResponse);
+
+        // Assert
+        $this->assertInstanceOf(TokensResponse::class, $tokensResponse);
     }
 }
