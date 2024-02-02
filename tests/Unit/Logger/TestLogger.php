8871d7a2bc6e3e80771d87443de73ad8d325a172
DudeMason <mason.eyre@homecarepulse.com>
2024-02-02 12:30:11 -0600

    Include in test case

---
+++ tests/Unit/Logger/TestLogger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pinnacle\OpenIdConnect\Tests\Unit\Logger;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\InvalidArgumentException;
+use Stringable;
+
+class TestLogger extends AbstractLogger
+{
+    public string $latestLevel = '';
+
+    public string $latestMessage = '';
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function log($level, string|Stringable $message, array $context = []): void
+    {
+        $this->latestLevel   = $level;
+        $this->latestMessage = $message;
+    }
+}
