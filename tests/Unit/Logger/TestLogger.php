4bc623683dad9a6ba716887ad7b14529df01fd20
DudeMason <mason.eyre@homecarepulse.com>
2024-02-02 12:31:46 -0600

    Formatting

--- tests/Unit/Logger/TestLogger.php
+++ tests/Unit/Logger/TestLogger.php
@@ -8,7 +8,7 @@ use Stringable;
 
 class TestLogger extends AbstractLogger
 {
-    public string $latestLevel = '';
+    public string $latestLevel   = '';
 
     public string $latestMessage = '';
 
